6ea8155ce383472a979807100497f79c5d64de98
pythontilk <noel.burkhardt@its-stuttgart.de>
2025-10-27 22:16:04 +0100

    Fix Next.js export config for Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
